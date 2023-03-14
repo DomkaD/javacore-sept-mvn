85520e3b1d48cf47a9207ccf542c2e9159d57851
DkDeniss <kvaskasdomkas@gmail.com>
2023-03-14 20:42:19 +0200

    hw

--- src/main/java/hw4/hw4_1/Main.java
+++ src/main/java/hw4/hw4_1/Main.java
@@ -9,17 +9,17 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-//        HashSet<User> userHashSet = new HashSet<>();
-//        userHashSet.add(new User(1,"vasya", "pupkin","afafafa@gmail.com",23,Gender.MALE, List.of(new Skill("java", 10)),
-//                new Car("lada",2000, 100)));
-//        userHashSet.add(new User(2,"oleg", "rara","afafafa@gmail.com",23,Gender.MALE, List.of(new Skill("react", 10)),
-//                new Car("audi",2002, 150)));
-//        userHashSet.add(new User(3,"sofia", "lalala","afafafa@gmail.com",23,Gender.FEMALE, List.of(new Skill("js", 10)),
-//                new Car("skoda",2003, 200)));
-//        userHashSet.add(new User(4,"olena", "gagaga","afafafa@gmail.com",23,Gender.FEMALE, List.of(new Skill("css", 10)),
-//                new Car("bmw",2005, 200)));
-//        userHashSet.removeIf(user -> user.getGender().equals(Gender.MALE));
-//        System.out.println(userHashSet);
+        HashSet<User> userHashSet = new HashSet<>();
+        userHashSet.add(new User(1,"vasya", "pupkin","afafafa@gmail.com",23,Gender.MALE, List.of(new Skill("java", 10)),
+                new Car("lada",2000, 100)));
+        userHashSet.add(new User(2,"oleg", "rara","afafafa@gmail.com",23,Gender.MALE, List.of(new Skill("react", 10)),
+                new Car("audi",2002, 150)));
+        userHashSet.add(new User(3,"sofia", "lalala","afafafa@gmail.com",23,Gender.FEMALE, List.of(new Skill("js", 10)),
+                new Car("skoda",2003, 200)));
+        userHashSet.add(new User(4,"olena", "gagaga","afafafa@gmail.com",23,Gender.FEMALE, List.of(new Skill("css", 10)),
+                new Car("bmw",2005, 200)));
+        userHashSet.removeIf(user -> user.getGender().equals(Gender.MALE));
+        System.out.println(userHashSet);
 
         TreeSet<User> userTreeSet = new TreeSet<>();
         userTreeSet.add(new User(1,"vasya", "pupkin","afafafa@gmail.com",23,Gender.MALE, List.of(new Skill("java", 10)),
@@ -30,7 +30,6 @@ public class Main {
                 new Car("skoda",2003, 200)));
         userTreeSet.add(new User(4,"olena", "gagaga","afafafa@gmail.com",23,Gender.FEMALE, List.of(new Skill("css", 10)),
                 new Car("bmw",2005, 200)));
-        userTreeSet.stream().collect(Collectors.toSet());
 
         System.out.println(userTreeSet);
 
